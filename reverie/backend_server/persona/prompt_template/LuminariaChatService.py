a07ea5516af732837be4d6918ec9094d1fcd77bd
Igor Novikov <septagramm@gmail.com>
2024-01-18 22:05:14 +0400

    [#23] Implement debug cache

--- reverie/backend_server/persona/prompt_template/LuminariaChatService.py
+++ reverie/backend_server/persona/prompt_template/LuminariaChatService.py
@@ -1,6 +1,9 @@
 from typing import List, Dict, Optional
 from semantic_kernel.connectors.ai.open_ai import OpenAIChatCompletion
 from utils import system_prompt
+from termcolor import colored
+
+from persona.prompt_template.DebugCache import DebugCache
 
 class LuminariaChatService(OpenAIChatCompletion):
   # System message to be included in every request
@@ -14,6 +17,8 @@ class LuminariaChatService(OpenAIChatCompletion):
     messages_with_system_message = [self._SYSTEM_MESSAGE] + (messages or [])
     if prompt:
       messages_with_system_message.append({'role': 'user', 'content': prompt})
+    print(colored(prompt, 'light_blue'))
     # Send the request with the system message included
-    return await super()._send_request(messages=messages_with_system_message, **kwargs)
-
+    response = await DebugCache.read(prompt) or await super()._send_request(messages=messages_with_system_message, **kwargs)
+    DebugCache.write(prompt, response)
+    return response
